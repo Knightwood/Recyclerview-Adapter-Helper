818525c22d870307177e45aaf6ef4fa54650cda0
KnightWood <33772264+Knightwood@users.noreply.github.com>
2023-04-19 19:31:53 +0800

    bug fix

--- RecyclerViewNeko/src/main/java/com/kiylx/recyclerviewneko/nekoadapter/config/LoadStatusConfig.kt
+++ RecyclerViewNeko/src/main/java/com/kiylx/recyclerviewneko/nekoadapter/config/LoadStatusConfig.kt
@@ -1,8 +1,11 @@
 package com.kiylx.recyclerviewneko.nekoadapter.config
 
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.ConcatAdapter
 import com.kiylx.recyclerviewneko.nekoadapter.NekoPagingStatusAdapter
 import com.kiylx.recyclerviewneko.nekoadapter.PagingStatusConfig
+import com.kiylx.recyclerviewneko.utils.RecyclerViewScrollListener
+import com.kiylx.recyclerviewneko.utils.addOnScrollListener
 
 class LoadStatusConfig<T : Any, N : BaseConfig<T>>(
     val config: N,//需要添加footer或header的原始adapter
@@ -10,6 +13,7 @@ class LoadStatusConfig<T : Any, N : BaseConfig<T>>(
     var header: NekoPagingStatusAdapter? = null
     var footer: NekoPagingStatusAdapter? = null
     lateinit var concatAdapter: ConcatAdapter
+    var whenEnd: (() -> Unit)? = null
 
     /**
      * 给pagingAdapter添加状态加载状态item
@@ -35,7 +39,33 @@ class LoadStatusConfig<T : Any, N : BaseConfig<T>>(
         } else {
             concatAdapter = ConcatAdapter(header, config.iNekoAdapter, footer)
             config.rv.adapter = concatAdapter
+            config.rv.addOnScrollListener(object : RecyclerViewScrollListener() {
+                override fun onScrollToBottom() {
+                    whenEnd?.invoke()
+                }
+            })
         }
+    }
+
+    /**
+     * 设置当滑动到底部时的回调监听
+     */
+    fun whenScrollToEnd(block: () -> Unit) {
+        this.whenEnd = block
+    }
+
+    /**
+     * 通过此方法改变header的itemview状态
+     */
+    fun headerState(loadState: LoadState) {
+        header?.loadState = loadState
+    }
+
+    /**
+     * 通过此方法改变footer的itemview状态
+     */
+    fun footerState(loadState: LoadState) {
+        footer?.loadState = loadState
     }
 
 }
